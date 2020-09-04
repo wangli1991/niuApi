cc42087d841fa0d1c8089ed15556376600955394
wangli1991 <1049963248@qq.com>
2020-09-04 12:57:45 +0800

    更新接口

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: 王利
+ * @Date: 2020-07-02 19:47:13
+ * @LastEditors: 王利
+ * @LastEditTime: 2020-09-04 12:55:56
+ */
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
@@ -9,27 +15,6 @@ const RedisStore = require("connect-redis")(session);
 //基础分类信息列表
 const baseListRouter = require("./routes/base_list");
 
-const varietyRouter = require("./routes/variety");
-const hairColorRouter = require("./routes/hair_color");
-const birthCountRouter = require("./routes/birth_count");
-const birthStatusRouter = require("./routes/birth_status");
-const breastHealthStatusRouter = require("./routes/breast_health_status");
-const feedingModeRouter = require("./routes/feeding_mode");
-const healthEvaluationRouter = require("./routes/health_evaluation");
-const entryAgeRouter = require("./routes/entry_age");
-const fattenTimeRouter = require("./routes/fatten_time");
-const calfFeedingModeRouter = require("./routes/calf_feeding_mode");
-const userPropertyRouter = require("./routes/user_property");
-const breedRangeRouter = require("./routes/breed_range");
-const memberCountRouter = require("./routes/member_count");
-const breedFacilitiesRouter = require("./routes/breed_facilities");
-const jobPropertyRouter = require("./routes/job_property");
-const healthStatusRouter = require("./routes/health_status");
-const technicalLevelRouter = require("./routes/technical_level");
-const creditGradeRouter = require("./routes/credit_grade");
-const ownerInfoRouter = require("./routes/owner_info");
-const technicalGradeRouter = require("./routes/technical_grade");
-const educationRouter = require("./routes/education");
 const userRouter = require("./routes/user");
 const areaManageRouter = require("./routes/area_manage");
 const filesUploadRouter = require("./routes/files_upload");
@@ -66,27 +51,6 @@ app.use(
 //routes handler app
 app.use("/api/baseList", baseListRouter);
 
-app.use("/api/variety", varietyRouter);
-app.use("/api/hairColor", hairColorRouter);
-app.use("/api/birthCount", birthCountRouter);
-app.use("/api/birthStatus", birthStatusRouter);
-app.use("/api/breastHealthStatus", breastHealthStatusRouter);
-app.use("/api/feedingMode", feedingModeRouter);
-app.use("/api/healthEvaluation", healthEvaluationRouter);
-app.use("/api/entryAge", entryAgeRouter);
-app.use("/api/fattenTime", fattenTimeRouter);
-app.use("/api/calfFeedingMode", calfFeedingModeRouter);
-app.use("/api/userProperty", userPropertyRouter);
-app.use("/api/breedRange", breedRangeRouter);
-app.use("/api/memberCount", memberCountRouter);
-app.use("/api/breedFacilities", breedFacilitiesRouter);
-app.use("/api/jobProperty", jobPropertyRouter);
-app.use("/api/healthStatus", healthStatusRouter);
-app.use("/api/technicalLevel", technicalLevelRouter);
-app.use("/api/creditGrade", creditGradeRouter);
-app.use("/api/ownerInfo", ownerInfoRouter);
-app.use("/api/technicalGrade", technicalGradeRouter);
-app.use("/api/education", educationRouter);
 app.use("/api/user", userRouter);
 app.use("/api/area_manage", areaManageRouter);
 app.use("/api/files_upload", filesUploadRouter);
